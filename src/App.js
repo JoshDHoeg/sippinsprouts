36d298388a215d86da91c021df29c7ff426ee03d
Josh Hoeg <josh@revivedesignstudios.com>
2018-05-19 11:38:53 -0600

    attempting to get api working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import axios from 'axios';
+import PlantList from './components/PlantList.js'
+
+const axiosInstance = axios.create({
+  baseURL: 'https://phzmapi.org/',
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {zip: '20001'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  componentDidMount() {
+    const query = this.state.zip + '.json';
+    console.log(query);
+    
+    axiosInstance.get( query )
+    .then(res => {
+      console.log(res.data);
+      const zipdata = res.data;
+      this.setState({ zipdata });
+    })
+}
+
+  handleChange(event) {
+    console.log(event.target.zip)
+    this.setState({zip: event.target.zip});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.zip);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Sippin Sprouts</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.zip} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <PlantList zip={this.state.zip} />
       </div>
     );
   }
